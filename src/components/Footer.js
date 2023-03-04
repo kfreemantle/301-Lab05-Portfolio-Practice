9e1b1d50ee5406e5f2256b64df9f6608a937378c
Kyle Freemantle <kcfreemantle@gmail.com>
2023-03-04 11:24:32 -0800

    updated professional page, need to do work history

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -20,7 +20,7 @@ class Footer extends Component {
           <div className="row">
             <div className="col social-links">{networks}</div>
             <div className="col">
-              <div 
+              {/* <div 
                 onClick={() =>
                   this.props.applyPickedLanguage(
                     window.$primaryLanguage,
@@ -44,14 +44,14 @@ class Footer extends Component {
                   )
                 }
                 style={{ display: "inline" }}
-              >
-                <span
+              > */}
+                {/* <span
                   className="iconify language-icon"
                   data-icon="twemoji-flag-for-flag-poland"
                   data-inline="false"
                   id={window.$secondaryLanguageIconId}
-                ></span>
-              </div>
+                ></span> */}
+              {/* </div> */}
             </div>
             <div className="col">
               <div className="copyright py-4 text-center">
